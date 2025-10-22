fc7bb30acd49ca1d42662a3d36997b7d7af2ee55
d-tech99 <yatulhamdani99@gmail.com>
2025-10-23 05:55:38 +0700

    Add files via upload

---
+++ Traffic-Monitoring/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtIP = New System.Windows.Forms.TextBox()
+        Me.TxtUser = New System.Windows.Forms.TextBox()
+        Me.TxtPass = New System.Windows.Forms.TextBox()
+        Me.BtnConnect = New System.Windows.Forms.Button()
+        Me.LV1 = New System.Windows.Forms.ListView()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.LblStatus = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IP Address"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Username"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(33, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Password"
+        '
+        'TxtIP
+        '
+        Me.TxtIP.Location = New System.Drawing.Point(104, 24)
+        Me.TxtIP.Name = "TxtIP"
+        Me.TxtIP.Size = New System.Drawing.Size(179, 20)
+        Me.TxtIP.TabIndex = 3
+        '
+        'TxtUser
+        '
+        Me.TxtUser.Location = New System.Drawing.Point(104, 60)
+        Me.TxtUser.Name = "TxtUser"
+        Me.TxtUser.Size = New System.Drawing.Size(179, 20)
+        Me.TxtUser.TabIndex = 4
+        '
+        'TxtPass
+        '
+        Me.TxtPass.Location = New System.Drawing.Point(104, 94)
+        Me.TxtPass.Name = "TxtPass"
+        Me.TxtPass.Size = New System.Drawing.Size(179, 20)
+        Me.TxtPass.TabIndex = 5
+        '
+        'BtnConnect
+        '
+        Me.BtnConnect.Location = New System.Drawing.Point(36, 144)
+        Me.BtnConnect.Name = "BtnConnect"
+        Me.BtnConnect.Size = New System.Drawing.Size(96, 36)
+        Me.BtnConnect.TabIndex = 6
+        Me.BtnConnect.Text = "Connect"
+        Me.BtnConnect.UseVisualStyleBackColor = True
+        '
+        'LV1
+        '
+        Me.LV1.HideSelection = False
+        Me.LV1.Location = New System.Drawing.Point(36, 186)
+        Me.LV1.Name = "LV1"
+        Me.LV1.Size = New System.Drawing.Size(504, 368)
+        Me.LV1.TabIndex = 7
+        Me.LV1.UseCompatibleStateImageBehavior = False
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(36, 560)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(96, 40)
+        Me.BtnStart.TabIndex = 8
+        Me.BtnStart.Text = "Start Monitoring"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(150, 560)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(95, 40)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Stop Monitoring"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'LblStatus
+        '
+        Me.LblStatus.AutoSize = True
+        Me.LblStatus.Location = New System.Drawing.Point(33, 599)
+        Me.LblStatus.Name = "LblStatus"
+        Me.LblStatus.Size = New System.Drawing.Size(0, 13)
+        Me.LblStatus.TabIndex = 10
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 674)
+        Me.Controls.Add(Me.LblStatus)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Controls.Add(Me.LV1)
+        Me.Controls.Add(Me.BtnConnect)
+        Me.Controls.Add(Me.TxtPass)
+        Me.Controls.Add(Me.TxtUser)
+        Me.Controls.Add(Me.TxtIP)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtIP As TextBox
+    Friend WithEvents TxtUser As TextBox
+    Friend WithEvents TxtPass As TextBox
+    Friend WithEvents BtnConnect As Button
+    Friend WithEvents LV1 As ListView
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents LblStatus As Label
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Traffic-Monitoring/Form1.vb
@@ -0,0 +1,95 @@
+﻿Imports tik4net
+Public Class Form1
+    Private connection As ITikConnection
+    Private monitoringActive As Boolean = False
+    Private lastRx As New Dictionary(Of String, Double)
+    Private lastTx As New Dictionary(Of String, Double)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Setup Listview
+        LV1.View = View.Details
+        LV1.Columns.Add("Interface", 150)
+        LV1.Columns.Add("RX (KB/s)", 120)
+        LV1.Columns.Add("TX (KB/s)", 120)
+        LV1.FullRowSelect = True
+        LV1.GridLines = True
+    End Sub
+
+    Private Sub BtnConnect_Click(sender As Object, e As EventArgs) Handles BtnConnect.Click
+        Try
+            connection = ConnectionFactory.CreateConnection(TikConnectionType.Api)
+            connection.Open(TxtIP.Text, TxtUser.Text, TxtPass.Text)
+            MessageBox.Show("Connected successfully!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            LblStatus.Text = "Connected to " & TxtIP.Text
+
+        Catch ex As Exception
+            MessageBox.Show("Failed to connect: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+        Try
+            If connection Is Nothing OrElse Not connection.IsOpened Then
+                MessageBox.Show("Please connect to the router first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            End If
+
+            monitoringActive = True
+            Timer1.Interval = 5000 ' 5 seconds
+            Timer1.Start()
+            LblStatus.Text = "Status: Monitoring Aktif"
+            LblStatus.ForeColor = Color.Green
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        monitoringActive = False
+        Timer1.Stop()
+        LblStatus.Text = "Status: Monitoring Nonaktif"
+        LblStatus.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Not monitoringActive Then Exit Sub
+
+        Try
+            Dim cmd = connection.CreateCommand("/interface/print")
+            Dim results = cmd.ExecuteList()
+
+            LV1.Items.Clear()
+
+            For Each iface In results
+                Dim name As String = iface.Words("name")
+                If name.Contains("pppoe") Then
+                    Dim rx As Double = CDbl(iface.Words("rx-byte"))
+                    Dim tx As Double = CDbl(iface.Words("tx-byte"))
+
+                    'hitung data dari pembacaan sebelummnya
+                    Dim rxRate As Double = 0
+                    Dim txRate As Double = 0
+
+                    If lastRx.ContainsKey(name) Then
+                        rxRate = (rx - lastRx(name)) / 1024 'KB/s
+                    End If
+                    If lastTx.ContainsKey(name) Then
+                        txRate = (tx - lastTx(name)) / 1024 'KB/s
+                    End If
+
+                    lastRx(name) = rx
+                    lastTx(name) = tx
+
+                    'tambahkan ke listview
+                    Dim item As New ListViewItem(name)
+                    item.SubItems.Add(rxRate.ToString("0.00") & " KB/s")
+                    item.SubItems.Add(txRate.ToString("0.00") & " KB/s")
+                    LV1.Items.Add(item)
+                End If
+            Next
+        Catch ex As Exception
+            LblStatus.Text = "Error: " & ex.Message
+        End Try
+    End Sub
+End Class
